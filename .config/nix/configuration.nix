64bcdf686dcd269e929009f9afbcbd21e541355f
Maximilian1996 <magnitudetwende@gmail.com>
2024-03-23 20:18:18 +0100

    added i3lock

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -55,6 +55,7 @@ in
 
   services.xserver.enable = true;
   services.xserver.windowManager.i3.enable = true;
+  programs.i3lock.enable = true;
 
   hardware.bluetooth.enable = true; # Enables bluetooth support
   #hardware.bluetooth.powerOnboot = true; # does not work
